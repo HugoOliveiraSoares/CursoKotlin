0db1984aefcb159c0fc26787e0dafb2545fe484e
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-03 11:05:09 -0300

    Criando variáveis

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    println("Bem vindo ao ByteBank!")
+
+//    Você pode definir essa variável tanto como mutável (var) como imutável (val).
+    val titular = "Hugo"
+
+//    String template
+    println("Titular $titular")
+
+}
