763a40d8c2f7bf51d729505d40672a75e43ada76
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-03 13:38:00 -0300

    Utilizando estruturas de repetição

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,20 +9,66 @@ fun main() {
       O tipo da variavel não é modificada durante a execução do programa.
 */
 
-    val titular: String = "Hugo"
-    val numeroConta: Int = 1000
-    var saldo = 0.0
-
-    saldo = 100 + 2.0
-    saldo += 200
-    saldo -= 2000
+    var i = 0
+    while (i < 5) {
+        val titular: String = "Hugo $i"
+        val numeroConta: Int = 1000 + i
+        var saldo = 10.0 + i
 
 //    String template
-    println("Titular $titular")
-    println("Numero conta $numeroConta")
-    println("Saldo conta $saldo")
+        println("Titular $titular")
+        println("Numero conta $numeroConta")
+        println("Saldo conta $saldo")
+        println("=====================")
+        i++
+    }
 
-    when{
+//    for (i in 5 downTo 1) {
+//
+//        if (i == 4)
+//            continue
+//
+//        val titular: String = "Hugo $i"
+//        val numeroConta: Int = 1000 + i
+//        var saldo = 10.0 + i
+//
+////    String template
+//        println("Titular $titular")
+//        println("Numero conta $numeroConta")
+//        println("Saldo conta $saldo")
+//        println("=====================")
+//
+////    testaCondicoes(saldo)
+//
+//    }
+
+    /*
+        //cria 5 contas
+        for (i in 1..5) {
+          //...
+        }
+
+        //cria 5 contas na ordem decrescente
+        for (i in 5 downTo 1) {
+          //...
+        }
+        //cria 3 contas de 2 em 2
+        for (i in 1..5 step 2) {
+          //...
+        }
+
+        //cria apenas 4 contas do 1 até 4
+        for (i in 1 until 5) {
+          //...
+        }
+
+     */
+
+}
+
+fun testaCondicoes(saldo: Double) {
+
+    when {
         saldo > 0.0 -> println("Conta é positiva")
         saldo == 0.0 -> println("Conta é neutra")
         else -> println("Conta é negativa")
