7f20517caaaa4f570985d4a4c86bcd0863f149ac
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-04 08:21:07 -0300

    Criando uma nova classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,44 +1,13 @@
 fun main() {
 
-    val contaHugo = Conta("Hugo", 1000)
-    contaHugo.deposita(250.0)
+    println("Bem vindo ao ByteBank!")
 
-    val contaFran = Conta("Fran", 1001)
-    contaFran.deposita(250.0)
+    val hugo = Funcionario("Hugo", "111111111", 1000.0, 0)
 
-    println(contaHugo.titular)
-    println(contaHugo.numeroConta)
-    println(contaHugo.saldo)
-    println()
-    println(contaFran.titular)
-    println(contaFran.numeroConta)
-    println(contaFran.saldo)
-
-    println("depositando na conta do Hugo")
-    contaHugo.deposita(50.0)
-    println(contaHugo.saldo)
-    println("depositando na conta da Fran")
-    contaFran.deposita(50.0)
-    println(contaFran.saldo)
-
-    println("Sacando na conta do Hugo")
-    contaHugo.saca(300.0)
-    println(contaHugo.saldo)
-    println("Sacando na conta da Fran")
-    contaFran.saca(50.0)
-    println(contaFran.saldo)
-
-    println("Tranferencia")
-
-    if (contaFran.tranfere(100.0, contaHugo))
-        println("Sucesso")
-    else
-        println("FALHA")
-
-    println(contaHugo.saldo)
-    println(contaFran.saldo)
-
-    println("TESTE")
-    println(contaHugo.saldo)
+    println("FUNCIONARIO")
+    println("nome ${hugo.nome}")
+    println("cpf ${hugo.cpf}")
+    println("salario ${hugo.salario}")
+    println("bonificação ${hugo.bonificacao()}")
 
 }
