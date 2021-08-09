a88cbaee971be4f69aefb34b54ef92db18f55ea2
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-09 11:45:09 -0300

    Array de objetos

--- src/main/kotlin/br/com/alura/bytebank/teste/arrays.kt
+++ src/main/kotlin/br/com/alura/bytebank/teste/arrays.kt
@@ -1,10 +1,42 @@
 package br.com.alura.bytebank.teste
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main() {
 //    introArrays()
 //    salarios()
 //    ranges()
-    idades()
+//    idades()
+    salariosComBigDecimal()
+
+}
+
+fun salariosComBigDecimal() {
+    val salarios = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000")
+
+    println(salarios.contentToString())
+
+    val aumento = "1.1".toBigDecimal()
+
+    val salariosComAumento = salarios
+        .map { salario ->
+            if (salario < "5000".toBigDecimal())
+                    salario + "500".toBigDecimal()
+            else
+                (salario * aumento).setScale(2, RoundingMode.UP)
+        }
+        .toTypedArray()
+
+    println(salariosComAumento.contentToString())
+
+}
+
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+
+    return Array<BigDecimal>(valores.size) {i ->
+        valores[i].toBigDecimal()
+    }
 
 }
 
