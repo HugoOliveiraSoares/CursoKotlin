cf86a902aaa305672937d7c1df2ec82ae765d6d6
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-09 08:48:18 -0300

    Lidando com referencias nulas

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,39 +1,11 @@
-import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import br.com.alura.bytebank.modelo.Endereco
-import br.com.alura.bytebank.teste.dizOi
-import br.com.alura.bytebank.teste.testaExpressao
 
 fun main() {
-//    println("início main")
-//    funcao1()
-//    println("fim main")
 
-    dizOi("Hugo") { parametro ->
-        println("Codigo de dentro da função")
-        return@dizOi "Oiiiii, ${parametro}"
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua um")
+    val logradouro: String? = enderecoNulo?.logradouro
+    enderecoNulo?.let { endereco: Endereco ->
+        println(endereco.logradouro.length)
     }
 
-}
-
-fun funcao1() {
-    println("início funcao1")
-    try {
-        funcao2()
-    } catch (e: SaldoInsuficienteException) {
-        e.printStackTrace()
-        println("SaldoInsuficienteException foi pegada")
-    }
-    println("fim funcao1")
-}
-
-fun funcao2() {
-    println("início funcao2")
-
-    for (i in 1..5) {
-        println(i)
-        val endereco = Any()
-        throw SaldoInsuficienteException()
-    }
-
-    println("fim funcao2")
 }
