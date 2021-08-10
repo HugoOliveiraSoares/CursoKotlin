875656c38a4c6690bcdf20706b8d70753c0c1726
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-10 09:33:19 -0300

    Listas imutáveis

--- src/main/kotlin/br/com/alura/bytebank/modelo/Prateleira.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Prateleira.kt
@@ -2,17 +2,15 @@ package br.com.alura.bytebank.modelo
 
 class Prateleira(
     val genero: String,
-    val livros: MutableList<Livro>
+    val livros: List<Livro>
 ) {
 
-    fun organizarPorAutor(): MutableList<Livro> {
-        livros.sortedBy { it.autor }
-        return livros
+    fun organizarPorAutor(): List<Livro> {
+        return livros.sortedBy { it.autor }
     }
 
-    fun organizarPorAnoPublicacao(): MutableList<Livro> {
-        livros.sortedBy { it.anoPublicacao }
-        return livros
+    fun organizarPorAnoPublicacao(): List<Livro> {
+        return livros.sortedBy { it.anoPublicacao }
     }
 
 }
