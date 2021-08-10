95933094ceff6c089d00899a4dcdea1276fa9c6c
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-10 14:13:49 -0300

    Leitura e filtros específicos de Map

--- src/main/kotlin/br/com/alura/bytebank/collections/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/collections/main.kt
@@ -2,8 +2,52 @@ package br.com.alura.bytebank.collections
 
 fun main() {
 
-    val pedidos = mutableMapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
-    testaMap(pedidos)
+    val pedidos = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 64.0),
+        3 to 50.0,
+        4 to 100.0,
+        5 to 150.0,
+        6 to 80.0
+    )
+
+//    val valorPedido = pedidos.getValue(5)
+//    println(valorPedido)
+    val message = pedidos.getOrElse(1) {
+        "Não tem o pedido"
+    }
+    println(message)
+
+    println(pedidos.getOrDefault(1, -1.0))
+    println(pedidos.getOrDefault(0, -1.0))
+
+    println(pedidos.keys)
+
+    for (numero in pedidos.keys)
+        println("Pedido $numero")
+
+    println(pedidos.values)
+
+    for (valor in pedidos.values)
+        println("Valor do pedido: $valor")
+
+    val pedidosFiltrados = pedidos.filter { (numero, valor) ->
+        numero % 2 == 0 && valor > 50.0
+    }
+
+    println(pedidosFiltrados)
+
+    val pedidosAcima = pedidos.filterValues { valor ->
+        valor > 70.0
+    }
+
+    println(pedidosAcima)
+
+    val pedidosPares = pedidos.filterKeys { numero ->
+        numero % 2 == 0
+    }
+
+    println(pedidosPares)
 
 }
 
