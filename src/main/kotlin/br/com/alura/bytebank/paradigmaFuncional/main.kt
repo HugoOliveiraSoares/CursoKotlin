f4f19192d55e5cae0a2be5d0bce264f4f6c0319d
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-11 09:48:32 -0300

    Expressão lambda e função anônima

--- src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
@@ -2,13 +2,32 @@ package br.com.alura.bytebank.paradigmaFuncional
 
 fun main() {
 
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
+
+    val minhaFuncaoLambda = {
+        println("Executa como lambda")
+    }
+
+    println(minhaFuncaoLambda())
+
+    val minhaFuncaoAnonima: () -> Unit = fun () {
+        println("Executa como anonima")
+    }
+
+    println(minhaFuncaoAnonima())
+
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasse = Teste()
+    println(minhaFuncaoClasse())
+}
+
+fun testaTipoFuncaoReferencia() {
     val minhaFuncao = ::teste
 
     println(minhaFuncao())
-
-    val minhaFuncaoClasse = Teste()
-    println(minhaFuncaoClasse())
-
 }
 
 fun teste() {
