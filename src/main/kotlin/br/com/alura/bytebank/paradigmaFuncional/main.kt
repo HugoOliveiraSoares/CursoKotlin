b321edf8ce21aa5d6d091c842f5d87e492122ca1
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-11 11:09:29 -0300

    Parâmetros e retornos na expressão lambda
    Lambdas com múltiplos retornos

--- src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
@@ -2,20 +2,36 @@ package br.com.alura.bytebank.paradigmaFuncional
 
 fun main() {
 
-    testaTipoFuncaoReferencia()
-    testaTipoFuncaoClasse()
+//    testaTipoFuncaoReferencia()
+//    testaTipoFuncaoClasse()
 
-//    val minhaFuncaoLambda = {
-//        println("Executa como lambda")
-//    }
-//
-//    println(minhaFuncaoLambda())
-//
-//    val minhaFuncaoAnonima: () -> Unit = fun () {
-//        println("Executa como anonima")
-//    }
-//
-//    println(minhaFuncaoAnonima())
+    val minhaFuncaoLambda = { a: Int, b: Int ->
+        a + b
+    }
+
+    println(minhaFuncaoLambda(15, 10))
+
+    val minhaFuncaoAnonima = fun (a: Int, b: Int): Int {
+        return a + b
+    }
+
+    println(minhaFuncaoAnonima(20, 10))
+
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0)
+            return@lambda salario + 50
+        return@lambda salario + 100.0
+    }
+
+    println(calculaBonificacao(1100.0))
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double{
+        if (salario > 1000.0)
+            return salario + 50
+        return salario + 100.0
+    }
+
+    println(calculaBonificacaoAnonima(1000.0))
 
 }
 
