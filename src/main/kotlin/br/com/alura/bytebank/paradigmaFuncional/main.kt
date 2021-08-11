b73cf3ffaac1599ab10a23f06074ca3d69cc1892
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-11 14:07:26 -0300

    Conhecendo as funções de alta ordem

--- src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/paradigmaFuncional/main.kt
@@ -2,50 +2,18 @@ package br.com.alura.bytebank.paradigmaFuncional
 
 fun main() {
 
-//    testaTipoFuncaoReferencia()
-//    testaTipoFuncaoClasse()
+//    higher order function
+    "".let(::testeRecebeString)
 
-    val minhaFuncaoLambda = { a: Int, b: Int ->
-        a + b
-    }
-
-    println(minhaFuncaoLambda(15, 10))
-
-    val minhaFuncaoAnonima = fun (a: Int, b: Int): Int {
-        return a + b
-    }
-
-    println(minhaFuncaoAnonima(20, 10))
-
-    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
-        if (salario > 1000.0)
-            return@lambda salario + 50
-        return@lambda salario + 100.0
-    }
-
-    println(calculaBonificacao(1100.0))
-
-    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double{
-        if (salario > 1000.0)
-            return salario + 50
-        return salario + 100.0
-    }
-
-    println(calculaBonificacaoAnonima(1000.0))
+    teste(1) {}
 
 }
 
-fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: (Int, Int) -> Int = Soma()
-    println(minhaFuncaoClasse(10, 10))
+fun testeRecebeString(valor: String){
+
 }
 
-fun testaTipoFuncaoReferencia() {
-    val minhaFuncao: (Int, Int) -> Int = ::soma
+//    higher order function
+fun teste(test: Int, block: () -> Unit): () -> Unit {
 
-    println(minhaFuncao(5, 10))
-}
-
-fun soma(a: Int, b: Int): Int {
-    return a + b
 }
